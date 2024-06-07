38825c48f75f4ca075a84bcd3840e3eee7b4746f
Pratham Kumar <kumarp@terralink-global.com>
2024-06-07 11:08:04 +0000

    First Commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("com.kpo.suppliersmatrixofresponsibility.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comkpo/suppliersmatrixofresponsibility/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
